86ef37b32c3a5c6c182efd0866fd044ddda86627
aharonthegamer4-png <aharonthegamer4@gmail.com>
2026-06-28 23:25:54 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from threading import Thread
 
-# יצירת שרת אינטרנט קטן
 app = Flask('')
 
 @app.route('/')
@@ -9,10 +8,10 @@ def home():
     return "Bot is running 24/7! Developed by Aaharon The Gamer"
 
 def run():
-    # הרצת השרת על פורט 8080 (סטנדרטי להוסטים)
+    # הרצת השרת
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    # הרצת השרת בנתיב נפרד (Thread) כדי שלא יתקע את הבוט של דיסקורד
+    # הפעלה בתוך Thread נפרד כדי שלא יתקע את הבוט של דיסקורד
     t = Thread(target=run)
     t.start()
